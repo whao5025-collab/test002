6143dd7c84f0866fb781ca475515de83e165cc6b
whao5025-collab <whao5025@gmail.com>
2025-12-01 06:59:34 +0800

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "next";
+
+const config: Config = {
+  content: [
+    // 关键点：一定要包含 src 目录下的所有文件
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
